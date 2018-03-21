ea521365d365f2e428bb672cd14e8810bb92a40c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 18:27:17 -0400

    Update iterable.jl

--- src/iterable.jl
+++ src/iterable.jl
@@ -7,7 +7,6 @@ start(x::CompoundPeriod) = 1
 done(x::CompoundPeriod, state) = state > length(x)
 next(x::CompoundPeriod, state) = x.periods[state], state+1
 
-reverseind(x::CompoundPeriod, idx) = length(x) - idx + 1
 
 eltype(x::ReverseCompoundPeriod) = Period
 length(x::ReverseCompoundPeriod) = length(x.periods)
@@ -26,4 +25,4 @@ start(x::Period) = 1
 done(x::Period, state) = state > 1
 next(x::Period, state) = x, state+1
 
-reverseind(x::Period, idx) = idx == 1 ? 1 : throw(BoundsError("$x [$idx]"))
+
