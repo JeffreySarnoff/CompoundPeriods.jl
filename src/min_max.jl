fac92575acd71d86e2a2db40bedd856d43aa897f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 15:24:20 -0400

    Update min_max.jl

--- src/min_max.jl
+++ src/min_max.jl
@@ -1,1 +1,14 @@
+typemax(::Type{CompoundPeriod}) = Year
+typemin(::Type{CompoundPeriod}) = Nanosecond
 
+typemax(::Type{P}) where {P<:Period} = P
+typemin(::Type{P}) where {P<:Period} = P
+
+# the extremal Periods with nonzero value
+max(x::CompoundPeriod) = isempty(x) ? x : x.periods[1]
+min(x::CompoundPeriod) = isempty(x) ? x : x.periods[end]
+minmax(x::CompoundPeriod) = min(x), max(x)
+
+max(x::Period) = x
+min(x::Period) = x
+minmax(x::Period) = x, x
