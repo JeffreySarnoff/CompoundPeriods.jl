754f517c86168e7f37afcd79e541e52fa02f4a2e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 15:19:33 -0400

    Update indexable.jl

--- src/indexable.jl
+++ src/indexable.jl
@@ -1,1 +1,11 @@
+function getindex(x::CompoundPeriod, idx::Int)
+    n = length(x.periods)
+    (idx == 0 || idx > n) && throw(BoundsError("$x [$idx]"))
+    return x.periods[idx]
+end
+
+function getindex(x::Period, idx::Int)
+    idx != 1 && throw(BoundsError("index must be 1"))
+    return x
+end
 
