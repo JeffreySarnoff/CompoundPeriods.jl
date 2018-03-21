85591719fa82b52420753cf5a66e6b4a2f1c8a8e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 18:52:42 -0400

    Update reversecompound.jl

--- src/reversecompound.jl
+++ src/reversecompound.jl
@@ -1,3 +1,7 @@
+struct ReverseCompoundPeriod <: AbstractTime
+   cperiod::CompoundPeriod
+end
+
 function string(rperiod::ReverseCompoundPeriod)
    str = "ReverseCompoundPeriod("
    for aperiod in rperiod
@@ -6,3 +10,17 @@ function string(rperiod::ReverseCompoundPeriod)
    str = string(str[1:end-2],")")
    return str
 end
+
+function show(io::IO, x::ReverseCompoundPeriod)
+   print(io, string(x))
+end
+
+if isdefined(Base, :stdout)
+    function show(x::ReverseCompoundPeriod)
+       print(Base.stdout, string(x)
+    end
+else
+    function show(x::ReverseCompoundPeriod)
+       print(Base.STDOUT, string(x)
+    end
+end      
