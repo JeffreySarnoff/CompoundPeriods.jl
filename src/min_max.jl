67dc309baa8647cd11ad3f8fc14bf78db91f5aae
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 16:38:08 -0400

    Update min_max.jl

--- src/min_max.jl
+++ src/min_max.jl
@@ -1,3 +1,8 @@
+# foreach nonempty Period in a CompoundPeriod, get type of Period
+typesof(x::CompoundPeriod) = map(typeof, x.periods)
+typesof(x::P) where {P<:Period} = (P,)
+
+
 # the extremal Periods with nonzero value
 max(x::CompoundPeriod) = isempty(x) ? x : x.periods[1]
 min(x::CompoundPeriod) = isempty(x) ? x : x.periods[end]
