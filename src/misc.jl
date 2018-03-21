96a34f35d8db881e73e65ca0614a14b6afbbd7c4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 19:46:41 -0400

    Update misc.jl

--- src/misc.jl
+++ src/misc.jl
@@ -12,6 +12,10 @@ max(x::CompoundPeriod) = isempty(x) ? x : x.periods[1]
 min(x::CompoundPeriod) = isempty(x) ? x : x.periods[end]
 minmax(x::CompoundPeriod) = min(x), max(x)
 
+max(x::ReverseCompoundPeriod) = isempty(x) ? x : x.periods[end]
+min(x::ReverseCompoundPeriod) = isempty(x) ? x : x.periods[1]
+minmax(x::ReverseCompoundPeriod) = min(x), max(x)
+
 max(x::Period) = x
 min(x::Period) = x
 minmax(x::Period) = x, x
