f80d5f0a1a2eee5bdc2bb2d4b05e18cee240ea0a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 19:53:56 -0400

    Update misc.jl

--- src/misc.jl
+++ src/misc.jl
@@ -1,7 +1,7 @@
 # exported interface to Dates.canonicalize and enhancements
 canonical(x::Period) = canonical(CompoundPeriod(x))
 canonical(x::CompoundPeriod) = canonicalize(x)
-canonical(x::ReverseCompoundPeriod) = canonicalize(x)
+canonical(x::ReverseCompoundPeriod) = ReverseCompoundPeriod(canonicalize(x.cperiod))
 
 # foreach nonempty Period in a CompoundPeriod, get type of Period
 typesof(x::CompoundPeriod) = map(typeof, x.periods)
@@ -12,8 +12,8 @@ max(x::CompoundPeriod) = isempty(x) ? x : x.periods[1]
 min(x::CompoundPeriod) = isempty(x) ? x : x.periods[end]
 minmax(x::CompoundPeriod) = min(x), max(x)
 
-max(x::ReverseCompoundPeriod) = isempty(x) ? x : x.periods[end]
-min(x::ReverseCompoundPeriod) = isempty(x) ? x : x.periods[1]
+max(x::ReverseCompoundPeriod) = isempty(x) ? x : x.cperiod.periods[end]
+min(x::ReverseCompoundPeriod) = isempty(x) ? x : x.cperiod.periods[1]
 minmax(x::ReverseCompoundPeriod) = min(x), max(x)
 
 max(x::Period) = x
