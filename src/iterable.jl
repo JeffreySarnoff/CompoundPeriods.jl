9a467ac04d829dae11eebb27003458cc6d62d324
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 15:20:44 -0400

    Update iterable.jl

--- src/iterable.jl
+++ src/iterable.jl
@@ -1,1 +1,17 @@
+# make CompoundPeriod iterable
 
+eltype(x::CompoundPeriod) = Period
+length(x::CompoundPeriod) = length(x.periods)
+
+start(x::CompoundPeriod) = 1
+done(x::CompoundPeriod, state) = state > length(x)
+next(x::CompoundPeriod, state) = x.periods[state], state+1
+
+# make Period iterable for interoperability with CompoundPeriod
+
+eltype(x::Period) = typeof(x)
+length(x::Period) = 1
+
+start(x::Period) = 1
+done(x::Period, state) = state > 1
+next(x::Period, state) = x, state+1
