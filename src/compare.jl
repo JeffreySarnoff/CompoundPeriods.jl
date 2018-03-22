95e76ebd74f58d9682ce065fecc21f5fb4fabbc2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 23:18:01 -0400

    Update compare.jl

--- src/compare.jl
+++ src/compare.jl
@@ -15,6 +15,7 @@
 Base.isless(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = x < y
 Base.isequal(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = x == y
 
+#=
 (==)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
     (Months(x) == Months(y)) && (Nanoseconds(x) == Nanoseconds(y))
 (!=)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
@@ -30,7 +31,7 @@ Base.isequal(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = x == y
 
 Base.isless(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} = x < y
 Base.isequal(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} = x == y
-
+=#
 #=
 (==)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
     Months(x) == Months(y) && Nanoseconds(x) == Nanoseconds(y)
