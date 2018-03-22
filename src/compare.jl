c30c7efa4686aaf4b5ea8cb7c23f3ea9528bd3e4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 21:58:56 -0400

    Update compare.jl

--- src/compare.jl
+++ src/compare.jl
@@ -15,3 +15,12 @@ Base.:(>=)(::Type{P1}, ::Type{P2}) where {P1<:Period, P2<:Period} = P1(1) > P2(1
 Base.isless(::Type{P1}, ::Type{P2}) where {P1<:Period, P2<:Period} = P1(1) < P2(1)
 Base.isequal(::Type{P1}, ::Type{P2}) where {P1<:Period, P2<:Period} = P1(1) == P2(1)
 
+Base.:(==)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = false
+Base.:(!=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = true
+Base.:(<)(x::P1, y::P2) where {P1<:CompundPeriod, P2<:Period} = max(x) < y
+Base.:(<=)(x::P1, y::P2) where {P1<:CompoudPeriod, P2<:Period} = max(x) <= y
+Base.:(>)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = max(x) > y
+Base.:(>=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = max(x) > y
+
+Base.isless(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = x < y
+Base.isequal(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = x == y
