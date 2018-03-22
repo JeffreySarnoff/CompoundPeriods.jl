23708e52010ada6b236ca11616c53c5f747766b5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 23:08:24 -0400

    Update compare.jl

--- src/compare.jl
+++ src/compare.jl
@@ -1,5 +1,3 @@
-const Periodal = Union{Period, CompoundPeriod}
-
 (==)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
     Months(x) == Months(y) && Nanoseconds(x) == Nanoseconds(y)
 (!=)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
