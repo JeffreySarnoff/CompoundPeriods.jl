073c2c3864e433af81817dfc5954cd59776637f5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-22 00:33:28 -0400

    Update canonical.jl

--- src/canonical.jl
+++ src/canonical.jl
@@ -1,47 +1,48 @@
-fldmod(x::Nanosecond) =
+@inline fldmod(x::Nanosecond) =
     map((f, x)->f(x), (Microsecond,Nanosecond), fldmod(x.value, NANOSECONDS_PER_MICROSECOND))
-fldmod(x::Microsecond) =
+@inline fldmod(x::Microsecond) =
     map((f, x)->f(x), (Millisecond,Microsecond), fldmod(x.value, MICROSECONDS_PER_MILLISECOND))
-fldmod(x::Millisecond) =
+@inline fldmod(x::Millisecond) =
     map((f, x)->f(x), (Second,Millisecond), fldmod(x.value, MILLISECONDS_PER_SECOND))
-fldmod(x::Second) =
+@inline fldmod(x::Second) =
     map((f, x)->f(x), (Minute,Second), fldmod(x.value, SECONDS_PER_MINUTE))
-fldmod(x::Minute) =
+@inline fldmod(x::Minute) =
     map((f, x)->f(x), (Hour,Minute), fldmod(x.value, MINUTES_PER_HOUR))
-fldmod(x::Hour) =
+@inline fldmod(x::Hour) =
     map((f, x)->f(x), (Day, Hour), fldmod(x.value, HOURS_PER_DAY))
-fldmod(x::Day) = x
+@inline fldmod(x::Day) = x
 
 function fldmod(cperiod::CompoundPeriod)
     cperiod = canonical(cperiod)
-    periodtypes = typesof(cperiod) 
+    periodtypes = typesof(cperiod)
+    presentperiods = Set(periodtypes)
     result = CompoundPeriod()
-    if !isempty(findall(periodtypes .== Nanosecond))
+    if Nanosecond in presentperiods
         microseconds, nanoseconds = fldmod(Nanosecond(cperiod))
         result += nanoseconds
         cperiod += microseconds
     end
-    if !isempty(findall(periodtypes .== Microsecond))
+    if Microsecond in presentperiods
         milliseconds, microseconds = fldmod(Microsecond(cperiod))
         result += microseconds
         cperiod += milliseconds
     end    
-    if !isempty(findall(periodtypes .== Millisecond))
+    if Millisecond in presentperiods
         seconds, milliseconds = fldmod(Millisecond(cperiod))
         result += milliseconds
         cperiod += seconds
     end
-    if !isempty(findall(periodtypes .== Second))
+    if Second in presentperiods
         minutes, seconds = fldmod(Second(cperiod))
         result += seconds
         cperiod += minutes
     end    
-    if !isempty(findall(periodtypes .== Minute))
+    if Minute in presentperiods
         hours, minutes = fldmod(Minute(cperiod))
         result += minutes
         cperiod += hours
     end    
-    if !isempty(findall(periodtypes .== Hour))
+    if Hour in presentperiods
        days, hours = fldmod(Hour(cperiod))
        result += hours
        cperiod += days
