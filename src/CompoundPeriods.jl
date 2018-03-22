a21e6ef1cf2884c6793a5a68174f6662cec282c6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 21:07:02 -0400

    Update CompoundPeriods.jl

--- src/CompoundPeriods.jl
+++ src/CompoundPeriods.jl
@@ -6,7 +6,7 @@ export Period, CompoundPeriod, ReverseCompoundPeriod,
    typesof, canonical
 
 import Base: start, done, next, getindex, lastindex, eltype, length,
-    min, max, minmax, reverseind, string, show, reverse
+    min, max, minmax, reverseind, string, show, reverse. fldmod
 
 import Dates: AbstractTime, Period, CompoundPeriod, canonicalize,
     Year, Month, Day, Hour, Minute, Second, Millisecond, Microsecond, Nanosecond
@@ -20,6 +20,7 @@ include("iterable.jl")
 include("indexable.jl")
 include("misc.jl")
 include("selector.jl")
+include("canonical.jl")
 include("compare.jl")
 
 
