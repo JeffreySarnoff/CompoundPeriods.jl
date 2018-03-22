ed35c2d1302a30f2ac4a897ec4c5abd0de067dae
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-22 00:09:39 -0400

    Update compare.jl

--- src/compare.jl
+++ src/compare.jl
@@ -1,10 +1,76 @@
+function (==)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period}
+    mx, my = Months(x), Months(y)
+    mx !== my && return false
+   
+    mintyp = Dates.periodisless(mintype(x)(1), P2(1)) ? mintype(x) : P2
+    typ = plural(mintyp)
+    xx = typ(x)
+    yy = typ(y)
+    xx.value == yy.value
+end
 
-(==)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
-    (Months(x) == Months(y)) && (Nanoseconds(x) == Nanoseconds(y))
-(!=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
-    (Months(x) != Months(y)) || (Nanoseconds(x) != Nanoseconds(y))
-(<)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
-    (Months(x) < Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) < Nanoseconds(y)))
+function (!=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period}
+    mx, my = Months(x), Months(y)
+    mx === my && return false
+   
+    mintyp = Dates.periodisless(mintype(x)(1), P2(1)) ? mintype(x) : P2
+    typ = plural(mintyp)
+    xx = typ(x)
+    yy = typ(y)
+    xx.value != yy.value
+end
+
+
+function (<)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period}
+    mx, my = Months(x), Months(y)
+    mx < my && return true
+    mx !== my && return false
+   
+    mintyp = Dates.periodisless(mintype(x)(1), P2(1)) ? mintype(x) : P2
+    typ = plural(mintyp)
+    xx = typ(x)
+    yy = typ(y)
+    xx.value < yy.value
+end
+
+function (<=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period}
+    mx, my = Months(x), Months(y)
+    mx < my && return true
+    mx > my && return false
+   
+    mintyp = Dates.periodisless(mintype(x)(1), P2(1)) ? mintype(x) : P2
+    typ = plural(mintyp)
+    xx = typ(x)
+    yy = typ(y)
+    xx.value <= yy.value
+end
+
+function (>)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period}
+    mx, my = Months(x), Months(y)
+    mx > my && return true
+    mx !== my && return false
+   
+    mintyp = Dates.periodisless(mintype(x)(1), P2(1)) ? mintype(x) : P2
+    typ = plural(mintyp)
+    xx = typ(x)
+    yy = typ(y)
+    xx.value > yy.value
+end
+
+function (>=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period}
+    mx, my = Months(x), Months(y)
+    mx > my && return true
+    mx < my && return false
+   
+    mintyp = Dates.periodisless(mintype(x)(1), P2(1)) ? mintype(x) : P2
+    typ = plural(mintyp)
+    xx = typ(x)
+    yy = typ(y)
+    xx.value >= yy.value
+end
+
+
+(Months(x) < Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) < Nanoseconds(y)))
 (<=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
     (Months(x) < Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) <= Nanoseconds(y)))
 (>)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
@@ -14,38 +80,3 @@
 
 Base.isless(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = x < y
 Base.isequal(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = x == y
-
-#=
-(==)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
-    (Months(x) == Months(y)) && (Nanoseconds(x) == Nanoseconds(y))
-(!=)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
-    (Months(x) != Months(y)) || (Nanoseconds(x) != Nanoseconds(y))
-(<)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
-    (Months(x) < Months(y)) || ()Months(x) == Months(y)) && (Nanoseconds(x) < Nanoseconds(y)))
-(<=)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
-    (Months(x) < Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) <= Nanoseconds(y)))
-(>)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
-    (Months(x) > Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) > Nanoseconds(y)))
-(>=)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
-    (Months(x) > Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) >= Nanoseconds(y)))
-
-Base.isless(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} = x < y
-Base.isequal(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} = x == y
-=#
-#=
-(==)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
-    Months(x) == Months(y) && Nanoseconds(x) == Nanoseconds(y)
-(!=)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
-    Months(x) != Months(y) || Nanoseconds(x) != Nanoseconds(y)
-(<)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
-    Months(x) < Months(y) || (Months(x) == Months(y) && Nanoseconds(x) < Nanoseconds(y))
-(<=)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
-    Months(x) < Months(y) || (Months(x) == Months(y) && Nanoseconds(x) <= Nanoseconds(y))
-(>)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
-    Months(x) > Months(y) || (Months(x) == Months(y) && Nanoseconds(x) > Nanoseconds(y))
-(>=)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
-    Months(x) > Months(y) || (Months(x) == Months(y) && Nanoseconds(x) >= Nanoseconds(y))
-
-isless(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} = x < y
-isequal(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} = x == y
-=#1
