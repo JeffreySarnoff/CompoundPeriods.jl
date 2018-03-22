00068a06f0d37ee6001d87ded152d102ab040d0b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 23:16:47 -0400

    Update compare.jl

--- src/compare.jl
+++ src/compare.jl
@@ -1,3 +1,37 @@
+
+(==)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
+    (Months(x) == Months(y)) && (Nanoseconds(x) == Nanoseconds(y))
+(!=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
+    (Months(x) != Months(y)) || (Nanoseconds(x) != Nanoseconds(y))
+(<)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
+    (Months(x) < Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) < Nanoseconds(y)))
+(<=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
+    (Months(x) < Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) <= Nanoseconds(y)))
+(>)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
+    (Months(x) > Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) > Nanoseconds(y)))
+(>=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} =
+    (Months(x) > Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) >= Nanoseconds(y)))
+
+Base.isless(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = x < y
+Base.isequal(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:Period} = x == y
+
+(==)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
+    (Months(x) == Months(y)) && (Nanoseconds(x) == Nanoseconds(y))
+(!=)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
+    (Months(x) != Months(y)) || (Nanoseconds(x) != Nanoseconds(y))
+(<)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
+    (Months(x) < Months(y)) || ()Months(x) == Months(y)) && (Nanoseconds(x) < Nanoseconds(y)))
+(<=)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
+    (Months(x) < Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) <= Nanoseconds(y)))
+(>)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
+    (Months(x) > Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) > Nanoseconds(y)))
+(>=)(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} =
+    (Months(x) > Months(y)) || ((Months(x) == Months(y)) && (Nanoseconds(x) >= Nanoseconds(y)))
+
+Base.isless(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} = x < y
+Base.isequal(x::P1, y::P2) where {P2<:CompoundPeriod, P1<:Period} = x == y
+
+#=
 (==)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
     Months(x) == Months(y) && Nanoseconds(x) == Nanoseconds(y)
 (!=)(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} =
@@ -13,3 +47,4 @@
 
 isless(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} = x < y
 isequal(x::P1, y::P2) where {P1<:Periodal, P2<:Periodal} = x == y
+=#1
