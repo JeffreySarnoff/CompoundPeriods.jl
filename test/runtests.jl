8deaa1ec1a03d60fe65133c67b28ce52b250edac
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 21:50:30 -0400

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+using CompoundPeriods, Dates
+using Test
+
+cperiod = Day(5) + Hour(17) + Minute(35)
+rperiod = reverse(cperiod)
+
+@test rperiod.cperiod == cperiod
+
+time_translation = canonical(Minute(-3600) + Second(22))
+temporal_algebra = fldmod(time_translation)
+@test temporal_algebra - Day(temporal_algebra) > Hour(12)
