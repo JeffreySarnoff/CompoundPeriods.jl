40d16868763491b2ac9c2a9656e39143243a7918
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 23:36:32 -0400

    Update pluralperiods.jl

--- src/pluralperiods.jl
+++ src/pluralperiods.jl
@@ -2,35 +2,33 @@ Nanoseconds(x::Period) = Nanosecond(x)
 Nanoseconds(x::CompoundPeriod) = sum(map(Nanosecond, x.periods))
 Nanoseconds(x::ReverseCompoundPeriod) = sum(map(Nanosecond, x.cperiod.periods))
 
+TimeUnits(x::CompoundPeriod) = sum(map(mintype(x), x.periods))
+TimeUnits(x::ReverseCompoundPeriod) = sum(map(mintype(x), x.cperiod.periods))
+TimeUnits(x::Period) = x
+
 Microseconds(x::T) where {T<:Nanosecond} = floor(x, Microsecond)
 Microseconds(x::T) where {T<:Period} = Microsecond(x)
-Microseconds(x::CompoundPeriod) = sum(map(Microsecond, x.periods))
-Microseconds(x::ReverseCompoundPeriod) = sum(map(Microsecond, x.cperiod.periods))
+Microseconds(x::T) where {T<:Periodous} = Microseconds(TimeUnits(x))
 
 Milliseconds(x::T) where {T<:Union{Nanosecond, Microsecond}} = floor(x, Millisecond)
 Milliseconds(x::T) where {T<:Period} = Millisecond(x)
-Milliseconds(x::CompoundPeriod) = sum(map(Millisecond, x.periods))
-Milliseconds(x::ReverseCompoundPeriod) = sum(map(Millisecond, x.cperiod.periods))
+Milliseconds(x::T) where {T<:Periodous} = Milliseconds(TimeUnits(x))
 
 Seconds(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond}} = floor(x, Second)
 Seconds(x::T) where {T<:Period} = Second(x)
-Seconds(x::CompoundPeriod) = Second(sum(map(mintype(x), x.periods)))
-Seconds(x::ReverseCompoundPeriod) = Second(sum(map(mintype(x), x.cperiod.periods)))
+Seconds(x::T) where {T<:Periodous} = Seconds(TimeUnits(x))
 
 Minutes(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond, Second}} = floor(x, Minute)
 Minutes(x::T) where {T<:Period} = Minute(x)
-Minutes(x::CompoundPeriod) = Minute(sum(map(mintype(x), x.periods)))
-Minutes(x::ReverseCompoundPeriod) = Minute(sum(map(mintype(x), x.cperiod.periods)))
+Minutes(x::T) where {T<:Periodous} = Minutes(TimeUnits(x))
 
 Hours(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond, Second, Minute}} = floor(x, Hour)
 Hours(x::T) where {T<:Period} = Hour(x)
-Hours(x::CompoundPeriod) = Hour(sum(map(mintype(x), x.periods)))
-Hours(x::ReverseCompoundPeriod) = Hour(sum(map(mintype(x), x.cperiod.periods)))
+Hours(x::T) where {T<:Periodous} = Hourss(TimeUnits(x))
 
 Days(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond, Second, Minute, Hour}} = floor(x, Day)
 Days(x::T) where {T<:Period} = Day(x)
-Days(x::CompoundPeriod) = Day(sum(map(mintype(x), x.periods)))
-Days(x::ReverseCompoundPeriod) = Day(sum(map(mintype(x), x.cperiod.periods)))
+Days(x::T) where {T<:Periodous} = Days(TimeUnits(x))
 
 Months(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond, Second, Minute, Hour, Day}} = Month(0)
 Months(x::T) where {T<:Year} = floor(x, Month)
@@ -44,7 +42,4 @@ Years(x::T) where {T<:Period} = Year(x)
 Years(x::CompoundPeriod) = sum(map(Year, x.periods))
 Years(x::ReverseCompoundPeriod) = sum(map(Year, x.cperiod.periods))
 
-TimeUnits(x::CompoundPeriod) = sum(map(mintype(x), x.periods))
-TimeUnits(x::ReverseCompoundPeriod) = sum(map(mintype(x), x.cperiod.periods))
-TimeUnits(x::Period) = x
 
