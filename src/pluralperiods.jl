303c606507749b77187554f422386a38376b4483
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-09 20:23:11 -0400

    Update pluralperiods.jl

--- src/pluralperiods.jl
+++ src/pluralperiods.jl
@@ -14,7 +14,7 @@ Nanoseconds(x::Period) = Nanosecond(x)
 Nanoseconds(x::CompoundPeriod) = sum(map(Nanosecond, x.periods))
 Nanoseconds(x::ReverseCompoundPeriod) = sum(map(Nanosecond, x.cperiod.periods))
 
-nanoseconds(x::T) where {T<:Union{Period,Periodous}} = Nanoseconds(x).value
+nanoseconds(x::T) where {T<:Union{Period,Compounds}} = Nanoseconds(x).value
 
 TimeUnits(x::CompoundPeriod) = sum(map(mintype(x), x.periods))
 TimeUnits(x::ReverseCompoundPeriod) = sum(map(mintype(x), x.cperiod.periods))
@@ -22,45 +22,45 @@ TimeUnits(x::Period) = x
 
 Microseconds(x::T) where {T<:Nanosecond} = floor(x, Microsecond)
 Microseconds(x::T) where {T<:Period} = Microsecond(x)
-Microseconds(x::T) where {T<:Periodous} = Microseconds(TimeUnits(x))
+Microseconds(x::T) where {T<:Compounds} = Microseconds(TimeUnits(x))
 
-microseconds(x::T) where {T<:Union{Period,Periodous}} = Microseconds(x).value
+microseconds(x::T) where {T<:Union{Period,Compounds}} = Microseconds(x).value
 
 Milliseconds(x::T) where {T<:Union{Nanosecond, Microsecond}} = floor(x, Millisecond)
 Milliseconds(x::T) where {T<:Period} = Millisecond(x)
-Milliseconds(x::T) where {T<:Periodous} = Milliseconds(TimeUnits(x))
+Milliseconds(x::T) where {T<:Compounds} = Milliseconds(TimeUnits(x))
 
-milliseconds(x::T) where {T<:Union{Period,Periodous}} = Milliseconds(x).value
+milliseconds(x::T) where {T<:Union{Period,Compounds}} = Milliseconds(x).value
 
 Seconds(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond}} = floor(x, Second)
 Seconds(x::T) where {T<:Period} = Second(x)
-Seconds(x::T) where {T<:Periodous} = Seconds(TimeUnits(x))
+Seconds(x::T) where {T<:Compounds} = Seconds(TimeUnits(x))
 
-seconds(x::T) where {T<:Union{Period,Periodous}} = Seconds(x).value
+seconds(x::T) where {T<:Union{Period,Compounds}} = Seconds(x).value
 
 Minutes(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond, Second}} = floor(x, Minute)
 Minutes(x::T) where {T<:Period} = Minute(x)
-Minutes(x::T) where {T<:Periodous} = Minutes(TimeUnits(x))
+Minutes(x::T) where {T<:Compounds} = Minutes(TimeUnits(x))
 
-minutes(x::T) where {T<:Union{Period,Periodous}} = Minutes(x).value
+minutes(x::T) where {T<:Union{Period,Compounds}} = Minutes(x).value
 
 Hours(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond, Second, Minute}} = floor(x, Hour)
 Hours(x::T) where {T<:Period} = Hour(x)
-Hours(x::T) where {T<:Periodous} = Hours(TimeUnits(x))
+Hours(x::T) where {T<:Compounds} = Hours(TimeUnits(x))
 
-hours(x::T) where {T<:Union{Period,Periodous}} = Hours(x).value
+hours(x::T) where {T<:Union{Period,Compounds}} = Hours(x).value
 
 Days(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond, Second, Minute, Hour}} = floor(x, Day)
 Days(x::T) where {T<:Period} = Day(x)
-Days(x::T) where {T<:Periodous} = Days(TimeUnits(x))
+Days(x::T) where {T<:Compounds} = Days(TimeUnits(x))
 
-days(x::T) where {T<:Union{Period,Periodous}} = Days(x).value
+days(x::T) where {T<:Union{Period,Compounds}} = Days(x).value
 
 Weeks(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond, Second, Minute, Hour, Day}} = floor(x, Week)
 Weeks(x::T) where {T<:Period} = Week(x)
-Weeks(x::T) where {T<:Periodous} = Weeks(TimeUnits(x))
+Weeks(x::T) where {T<:Compounds} = Weeks(TimeUnits(x))
 
-weeks(x::T) where {T<:Union{Period,Periodous}} = Weeks(x).value
+weeks(x::T) where {T<:Union{Period,Compounds}} = Weeks(x).value
 
 Months(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond, Second, Minute, Hour, Day}} = Month(0)
 Months(x::T) where {T<:Year} = floor(x, Month)
@@ -68,7 +68,7 @@ Months(x::T) where {T<:Period} = Month(x)
 Months(x::CompoundPeriod) = sum(map(Month, x.periods))
 Months(x::ReverseCompoundPeriod) = sum(map(Month, x.cperiod.periods))
 
-months(x::T) where {T<:Union{Period,Periodous}} = Months(x).value
+months(x::T) where {T<:Union{Period,Compounds}} = Months(x).value
 
 Years(x::T) where {T<:Union{Nanosecond, Microsecond, Millisecond, Second, Minute, Hour, Day}} = Year(0)
 Years(x::T) where {T<:Month} = floor(x, Year)
@@ -76,7 +76,7 @@ Years(x::T) where {T<:Period} = Year(x)
 Years(x::CompoundPeriod) = sum(map(Year, x.periods))
 Years(x::ReverseCompoundPeriod) = sum(map(Year, x.cperiod.periods))
 
-years(x::T) where {T<:Union{Period,Periodous}} = Years(x).value
+years(x::T) where {T<:Union{Period,Compounds}} = Years(x).value
 
 
 
