fdeab443a4c2317cbbef82b69b8c1c7944550e08
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-09 20:16:56 -0400

    Update selector.jl

--- src/selector.jl
+++ src/selector.jl
@@ -28,31 +28,3 @@ convert(::Type{Year}, x::T) where {T<:Diurnal} = Year(0)
 convert(::Type{Month}, x::T) where {T<:Diurnal} = Month(0)
 convert(::Type{T}, x::Year) where {T<:Diurnal} = T(0)
 convert(::Type{T}, x::Month) where {T<:Diurnal} = T(0)
-
-
-function isolate_days(cperiod::CompoundPeriod)
-    days = Day(cperiod)
-    cperiod = cperiod - days
-    return days, cperiod
-end
-
-function isolate_yearsmonths(cperiod::CompoundPeriod)
-    years = Year(cperiod)
-    months = Month(cperiod)
-    cperiod = cperiod - years
-    cperiod = cperiod - months
-    yearsmonths = years + months
-    return yearsmonths, cperiod  
-end
-
-function isolate_yearsmonths_days(cperiod::CompoundPeriod)
-    yearsmonths, cperiod = isolate_yearsmonths(cperiod)
-    days, cperiod = isolate_days(cperiod)
-    return yearsmonths, days, cperiod
-end
-
-function isolate_yearsmonthsdays(cperiod::CompoundPeriod)
-    yearsmonths, cperiod = isolate_yearsmonths(cperiod)
-    days, cperiod = isolate_days(cperiod)
-    return yearsmonths+days, cperiod
-end
