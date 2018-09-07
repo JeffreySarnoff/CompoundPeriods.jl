cf9f1e7773255fc4529e58482dd86539cd8bb519
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-07 04:26:20 -0400

    Update iterable.jl

--- src/iterable.jl
+++ src/iterable.jl
@@ -7,10 +7,12 @@ eltype(x::ReverseCompoundPeriod) = Period
 length(x::ReverseCompoundPeriod) = length(x.cperiod.periods)
 
 Base.iterate(x::CompoundPeriod) = 1
-Base.iterate(x::CompoundPeriod, state=(length(x), 1)) = state[2] <= state[1] ? x.periods[state], (state[1], state[2]+1) : nothing
+Base.iterate(x::CompoundPeriod, state=(length(x), 1)) = 
+    state[2] <= state[1] ? (x.periods[state], (state[1], state[2]+1)) : nothing
 
-Base.iterate(x::ReverseCompoundPeriod) =length(x.cperiod.periods)
-Base.iterate(x::ReverseCompoundPeriod, state=(length(x), length(x))) = state[2] > 0 ? x.cperiod.periods[state], (state[1], state[2]-1) : nothing
+Base.iterate(x::ReverseCompoundPeriod) = length(x.cperiod.periods)
+Base.iterate(x::ReverseCompoundPeriod, state=(length(x), length(x))) =
+    state[2] > 0 ? (x.cperiod.periods[state], (state[1], state[2]-1)) : nothing
 
 
 # make Period iterable for interoperability with CompoundPeriod
