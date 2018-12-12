9b537fa114d5fe0c6b0a45329bf85f26f2b15074
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-11 21:40:52 -0500

    Update iterable.jl

--- src/iterable.jl
+++ src/iterable.jl
@@ -6,13 +6,27 @@ length(x::CompoundPeriod) = length(x.periods)
 eltype(x::ReverseCompoundPeriod) = Period
 length(x::ReverseCompoundPeriod) = length(x.cperiod.periods)
 
-Base.iterate(x::CompoundPeriod) = 1
-Base.iterate(x::CompoundPeriod, state=(length(x), 1)) = 
-    state[2] <= state[1] ? (x.periods[state], (state[1], state[2]+1)) : nothing
+function Base.iterate(x::CompoundPeriod)
+    iszero(length(x)) && return nothing
+    return (x.periods[1], 1)
+end
 
-Base.iterate(x::ReverseCompoundPeriod) = length(x.cperiod.periods)
-Base.iterate(x::ReverseCompoundPeriod, state=(length(x), length(x))) =
-    state[2] > 0 ? (x.cperiod.periods[state], (state[1], state[2]-1)) : nothing
+function Base.iterate(x::CompoundPeriod, state::Int)
+    state === length(x) && return nothing
+    state += 1
+    return (x.periods[state], state)
+end
+
+function Base.iterate(x::ReverseCompoundPeriod)
+    iszero(length(x)) && return nothing
+    return (x.cperiod.periods[1], 1)
+end
+
+function Base.iterate(x::ReverseCompoundPeriod, state::Int)
+    state === length(x) && return nothing
+    state += 1
+    return (x.cperiod.periods[state], state)
+end
 
 
 # make Period iterable for interoperability with CompoundPeriod
@@ -21,6 +35,6 @@ eltype(x::Period) = typeof(x)
 length(x::Period) = 1
 
 Base.iterate(x::Period) = 1
-Base.iterate(x::Period, state=1) = state > 1 ? nothing : x, 2
+Base.iterate(x::Period, state::Int=1) = state > 1 ? nothing : x, 2
 
 
