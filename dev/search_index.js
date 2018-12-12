203a4ecafc21453833a3bc0d935eb9e2d91e1cbc
autodocs <autodocs>
2018-12-12 05:30:27 +0000

    build based on 5dd1b09

--- dev/search_index.js
+++ dev/search_index.js
@@ -5,23 +5,55 @@ var documenterSearchIndex = {"docs": [
     "page": "Overview",
     "title": "Overview",
     "category": "page",
-    "text": "These examples show the enhanced capabilities.julia> using Dates, CompoundPeriods\n\njulia> cperiod = Day(2) - Hour(18) + Second(3605)\n2 days, -18 hours, 3605 seconds\n\njulia> Day(cperiod), Hour(cperiod), Second(cperiod)\n(2 days, -18 hours, 3605 seconds)\n\njulia> day(cperiod), hour(cperiod), second(cperiod)\n(2, -18, 3605)\n\njulia> cperiod = canonical(cperiod)\n1 day, 7 hours, 5 seconds\n\njulia> Day(cperiod), Hour(cperiod), Second(cperiod)\n(1 day, 7 hours, 5 seconds)\n\njulia> day(cperiod), hour(cperiod), second(cperiod)\n(1, 7, 5)\n\njulia> cperiod[1], cperiod[end]\n1 day, 5 secondsjulia> using CompoundPeriods, Dates\n\njulia> cperiod = Day(5) + Hour(17) + Minute(35)\n5 days, 17 hours, 35 minutes\n\njulia> rperiod = reverse(cperiod)\n35 minutes, 17 hours, 5 days\n\njulia> [typeof(aperiod) for aperiod in cperiod]\n3-element Array{DataType,1}:\n Day   \n Hour  \n Minute\n \njulia> result = [];\n\njulia> for aperiod in rperiod\n            push!(result,(aperiod, aperiod.value))\n       end\n\njulia> result\n3-element Array{Any,1}:\n (35 minutes, 35)\n (17 hours, 17)  \n (5 days, 5)     julia>  time_translation = canonical(Minute(-3600) + Second(22))\n-2 days, -11 hours, -59 minutes, -38 seconds\n\njulia> temporal_algebra = fldmod(time_translation)\n-3 days, 12 hours, 22 seconds\n\njulia> cperiod = canonical(Day(2)-Hour(18)+Second(3605))\n1 day, 7 hours, 5 seconds\n\njulia> Second(cperiod), Minute(cperiod), Hour(cperiod), Day(cperiod)\n(5 seconds, 0 minutes, 7 hours, 1 day)\n\njulia> Seconds(cperiod), Minutes(cperiod), Hours(cperiod), Days(cperiod)\n(111605 seconds, 1860 minutes, 31 hours, 1 day)\n\njulia> TimeUnits(Day(2)+Hour(12))\n60 hours\n\njulia> TimeUnits(Minute(10)+Microsecond(100))\n600000100 microsecondsjulia> using Dates, CompoundPeriods\n\njulia> dyhr = Day(5) + Hour(79)\n5 days, 79 hours\n\njulia> dyhr = canonical(dyhr)\n1 week, 1 day, 7 hours\n\njulia> mnsc = canonical(Minute(-3600) + Second(22))\n-2 days, -11 hours, -59 minutes, -38 seconds\n\njulia> adatetime = DateTime(\"2004-03-02\")\n2004-03-02T00:00:00\n\njulia> Time(adatetime) + Nanosecond(4321)\n00:00:00.000004321\n\njulia> adatetime + mnsc\n2004-02-28T12:00:22julia> using CompoundPeriods, Dates\n\njulia> cperiod = Day(5) + Hour(17) + Minute(35)\n5 days, 17 hours, 35 minutes\n\njulia> rperiod = reverse(cperiod)\n35 minutes, 17 hours, 5 days\n\njulia> cperiod == reverse(rperiod)\ntrue\n\njulia> min(cperiod), max(rperiod)\n(35 minutes, 5 days)\n\njulia> minmax(cperiod) == minmax(rperiod)\ntruejulia> using Dates, CompoundPeriods\n\njulia> dtm = DateTime(\"2011-02-04T10:11:12.345\")\n2011-02-04T10:11:12.345\n\njulia> dt, tm = Date(dtm), Time(dtm)\n(2011-02-04, 10:11:12.345)\n\njulia> dt, CompoundPeriod(dt), dt == Date(CompoundPeriod(dt))\n(2011-02-04, 2011 years, 2 months, 4 days, true)\n\njulia> tm, CompoundPeriod(tm), tm == Time(CompoundPeriod(tm))\n(10:11:12.345, 10 hours, 11 minutes, 12 seconds, 345 milliseconds, true)\n\njulia> CompoundPeriod(dtm)\n(2011 years, 2 months, 4 days, 10 hours, 11 minutes, 12 seconds, 345 milliseconds)\n\njulia> dtm == DateTime(CompoundPeriod(dtm))\ntrue"
+    "text": "Examples that show how to use the enhanced capabilities."
 },
 
 {
-    "location": "references/#",
-    "page": "References",
-    "title": "References",
+    "location": "example1/#",
+    "page": "Example 1",
+    "title": "Example 1",
     "category": "page",
-    "text": ""
+    "text": "julia> using Dates, CompoundPeriods\n\njulia> cperiod = Day(2) - Hour(18) + Second(3605)\n2 days, -18 hours, 3605 seconds\n\njulia> Day(cperiod), Hour(cperiod), Second(cperiod)\n(2 days, -18 hours, 3605 seconds)\n\njulia> day(cperiod), hour(cperiod), second(cperiod)\n(2, -18, 3605)\n\njulia> cperiod = canonical(cperiod)\n1 day, 7 hours, 5 seconds\n\njulia> Day(cperiod), Hour(cperiod), Second(cperiod)\n(1 day, 7 hours, 5 seconds)\n\njulia> day(cperiod), hour(cperiod), second(cperiod)\n(1, 7, 5)\n\njulia> cperiod[1], cperiod[end]\n1 day, 5 seconds"
 },
 
 {
-    "location": "documentindex/#",
-    "page": "Index",
-    "title": "Index",
+    "location": "example2/#",
+    "page": "Example 2",
+    "title": "Example 2",
     "category": "page",
-    "text": ""
+    "text": "julia> using CompoundPeriods, Dates\n\njulia> cperiod = Day(5) + Hour(17) + Minute(35)\n5 days, 17 hours, 35 minutes\n\njulia> rperiod = reverse(cperiod)\n35 minutes, 17 hours, 5 days\n\njulia> [typeof(aperiod) for aperiod in cperiod]\n3-element Array{DataType,1}:\n Day   \n Hour  \n Minute\n \njulia> result = [];\n\njulia> for aperiod in rperiod\n            push!(result,(aperiod, aperiod.value))\n       end\n\njulia> result\n3-element Array{Any,1}:\n (35 minutes, 35)\n (17 hours, 17)  \n (5 days, 5)     "
+},
+
+{
+    "location": "example3/#",
+    "page": "Example 3",
+    "title": "Example 3",
+    "category": "page",
+    "text": "julia>  time_translation = canonical(Minute(-3600) + Second(22))\n-2 days, -11 hours, -59 minutes, -38 seconds\n\njulia> temporal_algebra = fldmod(time_translation)\n-3 days, 12 hours, 22 seconds\n\njulia> cperiod = canonical(Day(2)-Hour(18)+Second(3605))\n1 day, 7 hours, 5 seconds\n\njulia> Second(cperiod), Minute(cperiod), Hour(cperiod), Day(cperiod)\n(5 seconds, 0 minutes, 7 hours, 1 day)\n\njulia> Seconds(cperiod), Minutes(cperiod), Hours(cperiod), Days(cperiod)\n(111605 seconds, 1860 minutes, 31 hours, 1 day)\n\njulia> TimeUnits(Day(2)+Hour(12))\n60 hours\n\njulia> TimeUnits(Minute(10)+Microsecond(100))\n600000100 microseconds"
+},
+
+{
+    "location": "example4/#",
+    "page": "Example 4",
+    "title": "Example 4",
+    "category": "page",
+    "text": "julia> using Dates, CompoundPeriods\n\njulia> dyhr = Day(5) + Hour(79)\n5 days, 79 hours\n\njulia> dyhr = canonical(dyhr)\n1 week, 1 day, 7 hours\n\njulia> mnsc = canonical(Minute(-3600) + Second(22))\n-2 days, -11 hours, -59 minutes, -38 seconds\n\njulia> adatetime = DateTime(\"2004-03-02\")\n2004-03-02T00:00:00\n\njulia> Time(adatetime) + Nanosecond(4321)\n00:00:00.000004321\n\njulia> adatetime + mnsc\n2004-02-28T12:00:22"
+},
+
+{
+    "location": "example5/#",
+    "page": "Example 5",
+    "title": "Example 5",
+    "category": "page",
+    "text": "julia> using CompoundPeriods, Dates\n\njulia> cperiod = Day(5) + Hour(17) + Minute(35)\n5 days, 17 hours, 35 minutes\n\njulia> rperiod = reverse(cperiod)\n35 minutes, 17 hours, 5 days\n\njulia> cperiod == reverse(rperiod)\ntrue\n\njulia> min(cperiod), max(rperiod)\n(35 minutes, 5 days)\n\njulia> minmax(cperiod) == minmax(rperiod)\ntrue"
+},
+
+{
+    "location": "example6/#",
+    "page": "Example 6",
+    "title": "Example 6",
+    "category": "page",
+    "text": "julia> using Dates, CompoundPeriods\n\njulia> dtm = DateTime(\"2011-02-04T10:11:12.345\")\n2011-02-04T10:11:12.345\n\njulia> dt, tm = Date(dtm), Time(dtm)\n(2011-02-04, 10:11:12.345)\n\njulia> dt, CompoundPeriod(dt), dt == Date(CompoundPeriod(dt))\n(2011-02-04, 2011 years, 2 months, 4 days, true)\n\njulia> tm, CompoundPeriod(tm), tm == Time(CompoundPeriod(tm))\n(10:11:12.345, 10 hours, 11 minutes, 12 seconds, 345 milliseconds, true)\n\njulia> CompoundPeriod(dtm)\n(2011 years, 2 months, 4 days, 10 hours, 11 minutes, 12 seconds, 345 milliseconds)\n\njulia> dtm == DateTime(CompoundPeriod(dtm))\ntrue"
 },
 
 ]}
