b427188c39160f81c128536ad73b57da49a47ee7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-06 00:40:59 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,8 @@ rperiod = reverse(cperiod)
 
 @test rperiod.cperiod == cperiod
 
-time_translation = canonical(Minute(-3600) + Second(22))
-temporal_algebra = fldmod(time_translation)
-@test (temporal_algebra - Day(temporal_algebra)) > Hour(12)
+cperiod = canonical(Minute(-3600) + Second(22))
+@test (cperiod - Day(cperiod)) > Hour(12)
 
 @test TimeUnits(Minute(10)+Microsecond(100)) == Microsecond(600000100)
 
