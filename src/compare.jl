d8a10e6ca62723d35af23fbf1fb912848f57bb60
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-11-18 00:40:59 -0500

    use `Base.` qualifier to avoid ambiguities

--- src/compare.jl
+++ src/compare.jl
@@ -1,11 +1,11 @@
-function (==)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
+function Base.:(==)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
     return Base.:(==)(canonical(x), canonical(y))
 end
-function (!=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
+function Base.:(!=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
     return Base.:(!=)(canonical(x), canonical(y))
 end
 
-function (<)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
+function Base.:(<)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
     cx, cy = canonical(x), canonical(y)
     xmonths, ymonths = Months(cx), Months(cy)
     xsecs, ysecs = Seconds(cx), Seconds(cy)
@@ -13,7 +13,7 @@ function (<)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
     return xmonths < ymonths || (xmonths == ymonths && (xsecs < ysecs || (xsecs == ysecs && xnanos < ynanos)))
 end
 
-function (>)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
+function Base.:(>)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
     cx, cy = canonical(x), canonical(y)
     xmonths, ymonths = Months(cx), Months(cy)
     xsecs, ysecs = Seconds(cx), Seconds(cy)
@@ -21,7 +21,7 @@ function (>)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
     return xmonths > ymonths || (xmonths == ymonths && (xsecs > ysecs || (xsecs == ysecs && xnanos > ynanos)))
 end
 
-function (<=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
+function Base.:(<=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
     cx, cy = canonical(x), canonical(y)
     xmonths, ymonths = Months(cx), Months(cy)
     xsecs, ysecs = Seconds(cx), Seconds(cy)
@@ -29,7 +29,7 @@ function (<=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
     return xmonths < ymonths || (xmonths == ymonths && (xsecs < ysecs || (xsecs == ysecs && xnanos <= ynanos)))
 end
 
-function (>=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
+function Base.:(>=)(x::P1, y::P2) where {P1<:CompoundPeriod, P2<:CompoundPeriod}
     cx, cy = canonical(x), canonical(y)
     xmonths, ymonths = Months(cx), Months(cy)
     xsecs, ysecs = Seconds(cx), Seconds(cy)
@@ -117,13 +117,16 @@ end
 const PeriodTypes = (:Year, :Month, :Week, :Day, :Hour, :Minute, :Second, :Millisecond, :Microsecond, :Nanosecond)
 
 for P in PeriodTypes
-    @eval isless(::Type{$P}, ::Type{$P}) = false
+    @eval Base.isless(::Type{$P}, ::Type{$P}) = false
+    @eval Base.isequal(::Type{$P}, ::Type{$P}) = true
 end
 for idx1 in 1:(length(PeriodTypes)-1)
     P1 = PeriodTypes[idx1]
     for idx2 in (idx1+1):length(PeriodTypes)
         P2 = PeriodTypes[idx2]
-        @eval isless(::Type{$P2}, ::Type{$P1}) = true
-        @eval isless(::Type{$P1}, ::Type{$P2}) = false
+        @eval Base.isless(::Type{$P2}, ::Type{$P1}) = true
+        @eval Base.isless(::Type{$P1}, ::Type{$P2}) = false
+        @eval Base.isequal(::Type{$P2}, ::Type{$P1}) = false
+        @eval Base.isequal(::Type{$P1}, ::Type{$P2}) = false
     end
 end
